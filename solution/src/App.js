db5d731ad9dbb3ea73f665f4ce9d7b979f22a210
Matthew Sorkin <mps621a@gmail.com>
2023-12-14 21:20:45 -0500

    finally, add new form to App.js

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+//import the new file
+import NewForm from './newForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* My new form is included below */}
+      <NewForm />
     </div>
   );
 }
