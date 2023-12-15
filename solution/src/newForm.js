63456feb7c232fafec46a8567d15a9802fa55e68
Matthew Sorkin <mps621a@gmail.com>
2023-12-14 21:11:20 -0500

    finished all form functions and display for form

--- solution/src/newForm.js
+++ solution/src/newForm.js
@@ -9,14 +9,14 @@ import { isNameValid, getLocations } from './mock-api/apis';
 //create the form component
 //need to validate name (isNameValid) and get locations (getLocations)
 const NewForm = () => {
-    //set up attributes of form and of corresponding table
+    //set up attributes/states of form and of corresponding table
     const [name, setName] = useState(''); //name field
     const [location, setLocation] = useState(''); //location field
-    const [locations, setLocations] = useState([]); //locations
-    const [isNameTaken, setIsNameTaken] = useState(false); //name taken
+    const [locations, setLocations] = useState([]); //locations from api.js
+    const [isNameTaken, setIsNameTaken] = useState(false); //checks if name is taken
     const [entries, setEntries] = useState([]); //table entries
 
-    //now use api to retrieve locations list
+    //now use api to retrieve locations list via effect hook
     useEffect(() => {
         getLocations().then(setLocations);
     }, []);
@@ -25,9 +25,10 @@ const NewForm = () => {
     const checkValidName = async (name) => {
         const nameIsValid = await isNameValid(name);
         setIsNameTaken(!nameIsValid);
-    };
+    }; //end checkValidName
 
-    const handleNameChange = (event) => {
+    //event handler for form for name input
+    const handleNameInput = (event) => {
         //retrieve name
         const newName = event.target.value;
         setName(newName);
@@ -38,9 +39,76 @@ const NewForm = () => {
             //after field cleared, need to restart validation
             setIsNameTaken(false);
         }
+    }; //end handleNameInput
+
+    //event handler for locations in dropdown
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
     };
-    
-    
+
+    //function to clear form
+    const handleFormClear = () => {
+        setName('');
+        setLocation('');
+        setIsNameTaken(false);
+    }; //end handleFormClear
+
+    //function for form submission
+    const handleFormAdd = (event) => {
+        event.preventDefault();
+        //perform checks for valid name and selected location
+        if (name && !isNameTaken && location) {
+            //add new entry to entries and clear form
+            setEntries([...entries, { name, location }]);
+            handleFormClear();
+        }
+    }; //end handleFormAdd
+
+
+    //finally, return
+    return (
+        <div className="form-component">
+            <form onSubmit={handleFormAdd}>
+                <div className="input-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        id="name"
+                        value={name}
+                        onChange={handleNameInput}
+                        className={isNameTaken ? 'input-error' : ''}
+                    />
+                    {/* Erorr message for taken name */}
+                    {isNameTaken && <div className="error">this name has already been taken</div>}
+                </div>
+                <div className="input-group">
+                    <label htmlFor="location">Location</label>
+                    {/* Location dropdown */}
+                    <select id="location" value={location} onChange={handleLocationChange}>
+                        {locations.map((loc) => (
+                            <option key={loc} value={loc}>{loc}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="buttons">
+                    {/* Buttons for clearing form and adding new entry */}
+                    <button type="button" onClick={handleFormClear}>Clear</button>
+                    <button type="submit" disabled={isNameTaken}>Add</button>
+                </div>
+            </form>
+            {/* Display area for entries */}
+            <div className="entries-display">
+                {entries.map((entry, index) => (
+                    <div key={index} className="entry">
+                        <span>{entry.name}</span>
+                        <span>{entry.location}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 
 
 } //end NewForm
+
+//export
+export default NewForm;
